a7851102092dceb6de01acb1a29f1f7fda5cd6f4
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2023-10-19 10:00:32 -0300

    Add files via upload

---
+++ my-first-web-api/src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -0,0 +1,37 @@
+package dio.web.api.repository;
+
+import dio.web.api.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+    public void save(Usuario usuario){
+        System.out.println("SAVE - Recebendo usuário da camada de repositório");
+        System.out.println(usuario);
+    }
+
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo usuário da camada de repositório");
+        System.out.println(usuario);
+    }
+
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+    }
+
+    public List<Usuario> listAll(){
+        System.out.println("LIST - Listando todos os usuários do sistema");
+        List<Usuario> usuarios= new ArrayList<>();
+        usuarios.add(new Usuario("gleyson","password"));
+        usuarios.add(new Usuario("Diego","kanye"));
+        return usuarios;
+    }
+
+    public Usuario findById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("Diego","kanye");
+    }
+}
